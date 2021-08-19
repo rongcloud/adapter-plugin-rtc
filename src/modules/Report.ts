40b95fff15d41b7739f9094c7cc259fcc71694dd
孔聪 <kongcong@rongcloud.cn>
2021-08-19 18:42:55 +0800

    feat: 支持 Report 模块批量接收声音状态数据

--- src/modules/Report.ts
+++ src/modules/Report.ts
@@ -1,11 +1,22 @@
 import { BasicModule } from './Basic'
 
 export interface IReportInitOptions {
-  spoke? (user: { id: string, stream: { audioLevel: number } }): void
+  /**
+   * 用于接收音量统计数据，当 useBatch 值为 true 时，数据将以数组的形式批量回传
+   * @param data
+   */
+  spoke? (data: { id: string, stream: { audioLevel: number } } | { id: string, stream: { audioLevel: number } }[]): void
+  /**
+   * 批量接收音量统计信息的开关，默认关闭
+   */
+  useBatch?: boolean
 }
 
 export interface IReportStartOptions {
-  frequency: number
+  /**
+   * 音量上报时间间隔，默认 1000ms，有效值 `300` - `1000`
+   */
+  frequency?: number,
 }
 
 export class Report extends BasicModule {
@@ -18,15 +29,19 @@ export class Report extends BasicModule {
   start (options?: IReportStartOptions) {
     this._options.spoke && this._ctrl.checkRoomThen(async room => {
       room.onAudioLevelChange((audioLevelReportList) => {
-        audioLevelReportList.forEach(item => {
-          const data = {
+        const list = audioLevelReportList.map(item => {
+          return {
             id: item.track.getUserId(),
             stream: {
               audioLevel: item.audioLevel
             }
           }
-          this._options.spoke?.(data)
         })
+        if (this._options?.useBatch) {
+          this._options.spoke?.(list)
+        } else {
+          list.forEach(item => this._options.spoke?.(item))
+        }
       }, options?.frequency)
     })
   }
